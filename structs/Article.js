b9cc9f36fe53b7e9a719c0604df11c1327860494
synzen <michael994664@gmail.com>
2018-08-30 23:29:35 -0400

    Fixed bug in regexOps, more integrated tests to be divied up to units

--- structs/Article.js
+++ structs/Article.js
@@ -3,111 +3,7 @@ const htmlConvert = require('html-to-text')
 const globalConfig = require('../util/configs.js')
 const BASE_REGEX_PHS = ['title', 'author', 'summary', 'description', 'guid', 'date']
 
-function regexReplace (string, searchOptions, replacement) {
-  if (typeof searchOptions !== 'object') throw new TypeError(`Expected RegexOp search key to have an object value, found ${typeof searchOptions} instead`)
-  const flags = !searchOptions.flags ? 'g' : searchOptions.flags.includes('g') ? searchOptions.flags : searchOptions.flags + 'g' // Global flag must be included to prevent infinite loop during .exec
-  try {
-    const matchIndex = searchOptions.match !== undefined ? parseInt(searchOptions.match, 10) : undefined
-    const groupNum = searchOptions.group !== undefined ? parseInt(searchOptions.group, 10) : undefined
-    const regExp = new RegExp(searchOptions.regex, flags)
-    const matches = []
-    let match
-    do { // Find everything that matches the search regex query and push it to matches.
-      match = regExp.exec(string)
-      if (match) matches.push(match)
-    } while (match)
-    match = matches[matchIndex || 0][groupNum || 0]
-
-    if (replacement !== undefined) {
-      if (matchIndex === undefined && groupNum === undefined) { // If no match or group is defined, replace every full match of the search in the original string
-        for (let item of matches) {
-          const exp = new RegExp(this.constructor._escapeRegExp(item[0]), flags)
-          string = string.replace(exp, replacement)
-        }
-      } else if (matchIndex && groupNum === undefined) { // If no group number is defined, use the full match of this particular match number in the original string
-        const exp = new RegExp(this.constructor._escapeRegExp(matches[matchIndex][0]), flags)
-        string = string.replace(exp, replacement)
-      } else {
-        const exp = new RegExp(this.constructor._escapeRegExp(matches[matchIndex][groupNum]), flags)
-        string = string.replace(exp, replacement)
-      }
-    } else string = match
-
-    return string
-  } catch (e) {
-    return e
-  }
-}
-
-function evalRegexConfig (feedConfig, text, placeholderName) {
-  const customPlaceholders = {}
-
-  if (Array.isArray(feedConfig.regexOps[placeholderName])) { // Eval regex if specified
-    if (Array.isArray(feedConfig.regexOps.disabled) && feedConfig.regexOps.disabled.length > 0) { // .disabled can be an array of disabled placeholders, or just a boolean to disable everything
-      for (let n of feedConfig.regexOps.disabled) { // Looping through strings of placeholders
-        if (n === placeholderName) return null // text
-      }
-    }
-
-    const phRegexOps = feedConfig.regexOps[placeholderName]
-    for (let regexOp of phRegexOps) { // Looping through each regexOp for a placeholder
-      if (regexOp.disabled === true || typeof regexOp.name !== 'string') continue
-
-      if (!customPlaceholders[regexOp.name]) customPlaceholders[regexOp.name] = text // Initialize with a value if it doesn't exist
-
-      const clone = Object.assign({}, customPlaceholders)
-
-      const modified = regexReplace(clone[regexOp.name], regexOp.search, regexOp.replacement)
-      if (typeof modified !== 'string') throw modified
-      else customPlaceholders[regexOp.name] = modified // newText = modified
-    }
-  } else return null
-  return customPlaceholders
-}
-
-function cleanup (feedConfig, text, imgSrcs, anchorLinks) {
-  if (!text) return ''
-
-  text = htmlConvert.fromString(text, {
-    tables: (feedConfig.formatTables !== undefined && typeof feedConfig.formatTables === 'boolean' ? feedConfig.formatTables : globalConfig.formatTables) === true ? true : [],
-    wordwrap: null,
-    ignoreHref: true,
-    noLinkBrackets: true,
-    format: {
-      image: node => {
-        const isStr = typeof node.attribs.src === 'string'
-        let link = isStr ? node.attribs.src.trim() : node.attribs.src
-        if (isStr && link.startsWith('//')) link = 'http:' + link
-        else if (isStr && !link.startsWith('http://') && !link.startsWith('https://')) link = 'http://' + link
-
-        if (Array.isArray(imgSrcs) && imgSrcs.length < 9 && isStr && link) imgSrcs.push(link)
-
-        let exist = true
-        const globalExistOption = globalConfig.imageLinksExistence
-        exist = globalExistOption
-        const specificExistOption = feedConfig.imageLinksExistence
-        exist = typeof specificExistOption !== 'boolean' ? exist : specificExistOption
-        if (!exist) return ''
-
-        return link
-      },
-      anchor: (node, fn, options) => {
-        const orig = fn(node.children, options)
-        if (!Array.isArray(anchorLinks)) return orig
-        const href = node.attribs.href ? node.attribs.href.trim() : ''
-        if (anchorLinks.length < 5 && href) anchorLinks.push(href)
-        return orig
-      }
-    }
-  })
-
-  text = text.replace(/\n\s*\n\s*\n/g, '\n\n') // Replace triple line breaks with double
-  const arr = text.split('\n')
-  for (let q = 0; q < arr.length; ++q) arr[q] = arr[q].replace(/\s+$/, '') // Remove trailing spaces
-  return arr.join('\n')
-}
-
-module.exports = class Article {
+class Article {
   constructor (raw, feedConfig = {}) {
     this.feedConfig = feedConfig
     this.raw = raw
@@ -117,14 +13,14 @@ module.exports = class Article {
     this.placeholdersForRegex = BASE_REGEX_PHS.slice()
     this.meta = raw.meta
     this.guid = raw.guid
-    this.author = raw.author ? cleanup(this.feedConfig, raw.author) : ''
+    this.author = raw.author ? this.constructor._cleanup(this.feedConfig, raw.author) : ''
     this.link = raw.link ? raw.link.split(' ')[0].trim() : '' // Sometimes HTML is appended at the end of links for some reason
     if (this.reddit && this.link.startsWith('/r/')) this.link = 'https://www.reddit.com' + this.link
 
     // Title
     this.titleImages = []
     this.titleAnchors = []
-    this.title = cleanup(this.feedConfig, raw.title, this.titleImages, this.titleAnchors)
+    this.title = this.constructor._cleanup(this.feedConfig, raw.title, this.titleImages, this.titleAnchors)
     for (let titleImgNum = 0; titleImgNum < this.titleImages.length; ++titleImgNum) {
       const term = `title:image${titleImgNum + 1}`
       this[term] = this.titleImages[titleImgNum]
@@ -164,7 +60,7 @@ module.exports = class Article {
     // Description and reddit-specific placeholders
     this.descriptionImages = []
     this.descriptionAnchors = []
-    this.description = this.youtube ? raw['media:group']['media:description']['#'] : cleanup(this.feedConfig, raw.description, this.descriptionImages, this.descriptionAnchors) // Account for youtube's description
+    this.description = this.youtube ? raw['media:group']['media:description']['#'] : this.constructor._cleanup(this.feedConfig, raw.description, this.descriptionImages, this.descriptionAnchors) // Account for youtube's description
     for (let desImgNum = 0; desImgNum < this.descriptionImages.length; ++desImgNum) {
       const term = `description:image${desImgNum + 1}`
       this[term] = this.descriptionImages[desImgNum]
@@ -179,7 +75,7 @@ module.exports = class Article {
     // Summary
     this.summaryImages = []
     this.summaryAnchors = []
-    this.summary = cleanup(this.feedConfig, raw.summary, this.summaryImages, this.summaryAnchors)
+    this.summary = this.constructor._cleanup(this.feedConfig, raw.summary, this.summaryImages, this.summaryAnchors)
     for (let sumImgNum = 0; sumImgNum < this.summaryImages.length; ++sumImgNum) {
       const term = `summary:image${sumImgNum + 1}`
       this[term] = this.summaryImages[sumImgNum]
@@ -217,7 +113,7 @@ module.exports = class Article {
     if (this.enabledRegex) {
       this.regexPlaceholders = {} // Each key is a validRegexPlaceholder, and their values are an object of named placeholders with the modified content
       for (let placeholderName in this.placeholdersForRegex) {
-        const regexResults = evalRegexConfig(this.feedConfig, this[placeholderName], placeholderName)
+        const regexResults = this.constructor._evalRegexConfig(this.feedConfig, this[placeholderName], placeholderName)
         this.regexPlaceholders[placeholderName] = regexResults
       }
     }
@@ -246,4 +142,111 @@ module.exports = class Article {
       }
     }
   }
+
+  static _cleanup (feedConfig, text, imgSrcs, anchorLinks) {
+    if (!text) return ''
+  
+    text = htmlConvert.fromString(text, {
+      tables: (feedConfig.formatTables !== undefined && typeof feedConfig.formatTables === 'boolean' ? feedConfig.formatTables : globalConfig.formatTables) === true ? true : [],
+      wordwrap: null,
+      ignoreHref: true,
+      noLinkBrackets: true,
+      format: {
+        image: node => {
+          const isStr = typeof node.attribs.src === 'string'
+          let link = isStr ? node.attribs.src.trim() : node.attribs.src
+          if (isStr && link.startsWith('//')) link = 'http:' + link
+          else if (isStr && !link.startsWith('http://') && !link.startsWith('https://')) link = 'http://' + link
+  
+          if (Array.isArray(imgSrcs) && imgSrcs.length < 9 && isStr && link) imgSrcs.push(link)
+  
+          let exist = true
+          const globalExistOption = globalConfig.imageLinksExistence
+          exist = globalExistOption
+          const specificExistOption = feedConfig.imageLinksExistence
+          exist = typeof specificExistOption !== 'boolean' ? exist : specificExistOption
+          if (!exist) return ''
+  
+          return link
+        },
+        anchor: (node, fn, options) => {
+          const orig = fn(node.children, options)
+          if (!Array.isArray(anchorLinks)) return orig
+          const href = node.attribs.href ? node.attribs.href.trim() : ''
+          if (anchorLinks.length < 5 && href) anchorLinks.push(href)
+          return orig
+        }
+      }
+    })
+  
+    text = text.replace(/\n\s*\n\s*\n/g, '\n\n') // Replace triple line breaks with double
+    const arr = text.split('\n')
+    for (let q = 0; q < arr.length; ++q) arr[q] = arr[q].replace(/\s+$/, '') // Remove trailing spaces
+    return arr.join('\n')
+  }
+
+  static _evalRegexConfig (feedConfig, text, placeholderName) {
+    const customPlaceholders = {}
+  
+    if (Array.isArray(feedConfig.regexOps[placeholderName])) { // Eval regex if specified
+      if (Array.isArray(feedConfig.regexOps.disabled) && feedConfig.regexOps.disabled.length > 0) { // .disabled can be an array of disabled placeholders, or just a boolean to disable everything
+        for (let n of feedConfig.regexOps.disabled) { // Looping through strings of placeholders
+          if (n === placeholderName) return null // text
+        }
+      }
+  
+      const phRegexOps = feedConfig.regexOps[placeholderName]
+      for (const regexOp of phRegexOps) { // Looping through each regexOp for a placeholder
+        if (regexOp.disabled === true || typeof regexOp.name !== 'string') continue
+  
+        if (!customPlaceholders[regexOp.name]) customPlaceholders[regexOp.name] = text // Initialize with a value if it doesn't exist
+  
+        const clone = Object.assign({}, customPlaceholders)
+  
+        const modified = Article._regexReplace(clone[regexOp.name], regexOp.search, regexOp.replacement)
+        if (modified instanceof Error) throw modified
+        else customPlaceholders[regexOp.name] = modified // newText = modified
+      }
+    } else return null
+    return customPlaceholders
+  }
+
+  static _regexReplace (string, searchOptions, replacement) {
+    if (typeof searchOptions !== 'object') throw new TypeError(`Expected RegexOp search key to have an object value, found ${typeof searchOptions} instead`)
+    const flags = !searchOptions.flags ? 'g' : searchOptions.flags.includes('g') ? searchOptions.flags : searchOptions.flags + 'g' // Global flag must be included to prevent infinite loop during .exec
+    try {
+      const matchIndex = searchOptions.match !== undefined ? parseInt(searchOptions.match, 10) : undefined
+      const groupNum = searchOptions.group !== undefined ? parseInt(searchOptions.group, 10) : undefined
+      const regExp = new RegExp(searchOptions.regex, flags)
+      const matches = []
+      let match
+      do { // Find everything that matches the search regex query and push it to matches.
+        match = regExp.exec(string)
+        if (match) matches.push(match)
+      } while (match)
+      if (matches.length === 0) return string
+      else match = matches[matchIndex || 0][groupNum || 0]
+  
+      if (replacement !== undefined) {
+        if (matchIndex === undefined && groupNum === undefined) { // If no match or group is defined, replace every full match of the search in the original string
+          for (let item of matches) {
+            const exp = new RegExp(Article._escapeRegExp(item[0]), flags)
+            string = string.replace(exp, replacement)
+          }
+        } else if (matchIndex && groupNum === undefined) { // If no group number is defined, use the full match of this particular match number in the original string
+          const exp = new RegExp(Article._escapeRegExp(matches[matchIndex][0]), flags)
+          string = string.replace(exp, replacement)
+        } else {
+          const exp = new RegExp(Article._escapeRegExp(matches[matchIndex][groupNum]), flags)
+          string = string.replace(exp, replacement)
+        }
+      } else string = match
+  
+      return string
+    } catch (e) {
+      return e
+    }
+  }
 }
+
+module.exports = Article
--- test/structs/Article.js
+++ test/structs/Article.js
@@ -6,12 +6,13 @@ const nock = require('nock')
 const fs = require('fs')
 const unconvertedArticle = JSON.parse(fs.readFileSync('./test/files/article.json'))
 const nestedImages = JSON.parse(fs.readFileSync('./test/files/nestedImages.json'))
+const regexOps = JSON.parse(fs.readFileSync('./test/files/regexOps.json'))
 
 describe('Article', function () {
   before(function () {
     process.env.feedwatch_test = true
   })
-  describe('escapeRegExp', function () {
+  describe('static ._escapeRegExp', function () {
     it('should escape regex properly', function () {
       const str = '\\ ^ $ * + ? . ( ) | { } [ ]'
       const escaped = Article._escapeRegExp(str)
@@ -19,7 +20,7 @@ describe('Article', function () {
       expect(regexp.test(str)).to.equal(true)
     })
   })
-  describe('findImages', function () {
+  describe('static ._findImages', function () {
     it('should put all images in a deeply nested object in an array', function () {
       const expectedImages = [ 'https://www.foobar.com/one.jpg',
         'https://www.foobar.com/three.jpeg',
@@ -34,5 +35,55 @@ describe('Article', function () {
       for (const url of results) expectedImages.splice(expectedImages.indexOf(url), 1)
       expect(expectedImages.length).to.equal(0)
     })
+  })
+
+  describe('static ._evalRegexConfig', function () {
+    it('should return null when the placeholder type is in the regexOps.disabled array', function () {
+      expect(Article._evalRegexConfig(regexOps, '', 'title')).to.be.a('null')
+    })
+    it('should return an empty object when there is only one regexOp for a placeholder type and it has disabled set to true', function () {
+      const results = Article._evalRegexConfig(regexOps, '', 'description')
+      expect(Object.keys(results).length).to.equal(0)
+      expect(results.constructor).to.equal(Object)
+    })
+    it('should replace the original text with content replaced if replacement key is defined', function () {
+      const inputText = 'foobar binge'
+      const results = Article._evalRegexConfig(regexOps, inputText, 'author')
+      expect(results.constructor).to.equal(Object)
+      expect(results).to.have.keys('authorname')
+      expect(results.authorname).to.equal(' binge')
+    })
+    it('should return the matched content if replacement key is not defined', function () {
+      const inputText = 'foobar binge'
+      const results = Article._evalRegexConfig(regexOps, inputText, 'authortwo')
+      expect(results.constructor).to.equal(Object)
+      expect(results).to.have.keys('authortwoname')
+      expect(results.authortwoname).to.equal('foobar')
+    })
+    it('should evaluate multiple regexOps properly', function () {
+      const inputText = 'foobar foobar2 foobar3'
+      const results = Article._evalRegexConfig(regexOps, inputText, 'summary')
+      expect(results.constructor).to.equal(Object)
+      expect(results).to.have.keys(['summarynameone', 'summarynametwo'])
+      expect(results.summarynameone).to.equal(' 2 3')
+      expect(results.summarynametwo).to.equal('foobar foobar3 foobar3')
+    })
+    it('should chain multiple regexOps properly', function () {
+      const inputText = 'foobar foobar2 foobar3'
+      const results = Article._evalRegexConfig(regexOps, inputText, 'summarytwo')
+      expect(results.constructor).to.equal(Object)
+      expect(results).to.have.keys('summarychained')
+      expect(results.summarychained).to.equal(' replaced2 3')
+    })
+    // describe('feedConfig with regexOps key "disabled" array has a field', function () {
+    //   it('should return null when the field is the input', function () {
+
+    //   })
+    //   it('should return an object when the field is not the input', function () {
+    //     const feedConfig = { regexOps: { disabled: ['description'], title: [] } }
+    //     expect(Article._evalRegexConfig(feedConfig, '', 'title')).to.be.a('object')
+    //   })
+    // })
+    // describe('feedConfig')
   })
 })
