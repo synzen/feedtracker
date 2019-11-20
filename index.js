f563fb4616eb959f23347711ac1632761483eea7
synzen <michael994664@gmail.com>
2019-11-19 22:48:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Fetcher = require('./structs/Fetcher.js')
 const Feed = require('./structs/Feed.js')
 const Schedule = require('./structs/Schedule.js')
+
 exports.Fetcher = Fetcher
 exports.Feed = Feed
 exports.Schedule = Schedule
